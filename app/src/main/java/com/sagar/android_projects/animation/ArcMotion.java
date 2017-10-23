9e4d30ee1200c97fb2e03434b48acdb29bf04fc4
sagar <snkumar.nayak@gmail.com>
2017-10-23 17:10:11 +0530

    arc motion.

--- app/src/main/java/com/sagar/android_projects/animation/ArcMotion.java
+++ app/src/main/java/com/sagar/android_projects/animation/ArcMotion.java
@@ -3,17 +3,20 @@ package com.sagar.android_projects.animation;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
 
-public class SharedElement extends AppCompatActivity {
+public class ArcMotion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shared_element);
+        setContentView(R.layout.activity_arc_motion);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getWindow().setSharedElementEnterTransition(new android.transition.ChangeBounds());
+        Transition transition = TransitionInflater.from(this).inflateTransition(R.transition.arc_motion);
+        getWindow().setSharedElementEnterTransition(transition);
     }
 
 }
--- app/src/main/java/com/sagar/android_projects/animation/MainActivity.java
+++ app/src/main/java/com/sagar/android_projects/animation/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     Button buttonNormalTransition;
     Button buttonSharedElement;
     ConstraintLayout constraintLayout;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         buttonNormalTransition = (Button) findViewById(R.id.normal_transition);
         buttonSharedElement = (Button) findViewById(R.id.shared_element_transition);
         constraintLayout = (ConstraintLayout) findViewById(R.id.constraint_layout_mainactivity);
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
 
         buttonNormalTransition.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,7 +47,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 getWindow().setSharedElementExitTransition(new android.transition.ChangeBounds());
-                startActivity(new Intent(MainActivity.this, SharedElement.class),ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,Pair.create((View)buttonSharedElement,ViewCompat.getTransitionName(buttonSharedElement))).toBundle());
+                startActivity(new Intent(MainActivity.this, SharedElement.class), ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, Pair.create((View) buttonSharedElement, ViewCompat.getTransitionName(buttonSharedElement))).toBundle());
+            }
+        });
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ArcMotion.class), ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, Pair.create((View) floatingActionButton, ViewCompat.getTransitionName(floatingActionButton))).toBundle());
             }
         });
     }
